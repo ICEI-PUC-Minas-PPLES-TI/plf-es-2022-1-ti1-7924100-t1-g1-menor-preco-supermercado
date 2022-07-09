d69a4009d4e7ddad3f30d39b17d7401313ee3e6a
jackmarry27 <103538583+jackmarry27@users.noreply.github.com>
2022-07-08 22:29:41 -0300

    Add files via upload

---
+++ code/Code/app.js
@@ -0,0 +1,735 @@
+// declara um conjunto inicial de produtos
+var rp_produtos_inicial = {
+  "data": [
+
+    {
+      "id": 1,
+      "nome": "Coca-Cola 2 l",
+      "marca": "Coca-Cola",
+      "categoria": "Bebidas"
+    },
+    {
+      "id": 2,
+      "nome": "Água Mineral com gás São Lourenço 1,25 l",
+      "marca": "São Lourenço",
+      "categoria": "Bebidas"
+    },
+    {
+      "id": 3,
+      "nome": "Guaraná Antarctica 2 l",
+      "marca": "Antarctica",
+      "categoria": "Bebidas"
+    },
+    {
+      "id": 4,
+      "nome": "Suco de Uva Tial 1 l",
+      "marca": "Tial",
+      "categoria": "Bebidas"
+    },
+    {
+      "id": 5,
+      "nome": "Red Bull lata 250ml",
+      "marca": "Red Bull",
+      "categoria": "Bebidas"
+    },
+    {
+      "id": 6,
+      "nome": "Heineken lata 473ml",
+      "marca": "Heineken",
+      "categoria": "Bebidas Alcoólicas"
+    },
+    {
+      "id": 7,
+      "nome": "Skol lata 473ml",
+      "marca": "Skol",
+      "categoria": "Bebidas Alcoólicas"
+    },
+    {
+      "id": 8,
+      "nome": "Stella Artois lata 473ml",
+      "marca": "Stella Artois",
+      "categoria": "Bebidas Alcoólicas"
+    },
+    {
+      "id": 9,
+      "nome": "Budweiser lata 473ml",
+      "marca": "Budweiser",
+      "categoria": "Bebidas Alcoólicas"
+    },
+    {
+      "id": 10,
+      "nome": "Itaipava lata 473ml",
+      "marca": "Itaipava",
+      "categoria": "Bebidas Alcoólicas"
+    },
+    {
+      "id": 11,
+      "nome": "Patinho Moído 500g",
+      "marca": "Supernosso",
+      "categoria": "Carnes, Aves e Peixes"
+    },
+    {
+      "id": 12,
+      "nome": "Filé Tilápia 500g",
+      "marca": "Supernosso",
+      "categoria": "Carnes, Aves e Peixes"
+    },
+    {
+      "id": 13,
+      "nome": "Galeto Temperado 400g",
+      "marca": "Supernosso",
+      "categoria": "Carnes, Aves e Peixes"
+    },
+    {
+      "id": 14,
+      "nome": "Empanado de frango Tekitos 300g",
+      "marca": "Seara",
+      "categoria": "Carnes, Aves e Peixes"
+    },
+    {
+      "id": 15,
+      "nome": "Hambúrguer de Carne Bovina Perdigão 672g",
+      "marca": "Perdigão",
+      "categoria": "Carnes, Aves e Peixes"
+    },
+    {
+      "id": 16,
+      "nome": "Batata Inglesa 1 kg",
+      "marca": "Supernosso",
+      "categoria": "Hortifrúti"
+    },
+    {
+      "id": 17,
+      "nome": "Laranja Pera Rio 1 kg",
+      "marca": "Supernosso",
+      "categoria": "Hortifrúti"
+    },
+    {
+      "id": 18,
+      "nome": "Maçã Gala 500g",
+      "marca": "Supernosso",
+      "categoria": "Hortifrúti"
+    },
+    {
+      "id": 19,
+      "nome": "Cebola Amarela 1 kg",
+      "marca": "Supernosso",
+      "categoria": "Hortifrúti"
+    },
+    {
+      "id": 20,
+      "nome": "Banana Prata 800 g",
+      "marca": "Supernosso",
+      "categoria": "Hortifrúti"
+    },
+    {
+      "id": 21,
+      "nome": "Sabão em Pó Omo 2,2kg",
+      "marca": "Omo",
+      "categoria": "Limpeza"
+    },
+    {
+      "id": 22,
+      "nome": "Detergente Líquido Limpol 500ml",
+      "marca": "Limpol",
+      "categoria": "Limpeza"
+    },
+    {
+      "id": 23,
+      "nome": "Detergente Líquido Ypê 500ml",
+      "marca": "Ypê",
+      "categoria": "Limpeza"
+    },
+    {
+      "id": 24,
+      "nome": "Lã de Aço Bombril Pacote com 8 Unidades 60g",
+      "marca": "Bombril",
+      "categoria": "Limpeza"
+    },
+    {
+      "id": 25,
+      "nome": "Amaciante Concentrado Comfort 1 l",
+      "marca": "Comfort",
+      "categoria": "Limpeza"
+    },
+    {
+      "id": 26,
+      "nome": "Papel Higiênico Neve Folha Dupla 30 Metros 16 Unidades",
+      "marca": "Neve",
+      "categoria": "Higiene e beleza"
+    },
+    {
+      "id": 27,
+      "nome": "Sabonete em Barra Nivea 85 g",
+      "marca": "Nivea",
+      "categoria": "Higiene e beleza"
+    },
+    {
+      "id": 28,
+      "nome": "Cotonetes Johnson&Johnson 150 Unidades",
+      "marca": "Johnson&Johnson",
+      "categoria": "Higiene e beleza"
+    },
+    {
+      "id": 29,
+      "nome": "Sabonete Líquido Fiorucci 500 ml",
+      "marca": "Fiorucci",
+      "categoria": "Higiene e beleza"
+    },
+    {
+      "id": 30,
+      "nome": "Desodorante Aerossol Rexona Men 150ml",
+      "marca": "Rexona Men",
+      "categoria": "Higiene e beleza"
+    }
+  ]
+}
+
+var rp_ListaAtiva_inicial = {
+  "data": [
+    {
+      "id": 1,
+      "quantidade": "0"
+    },
+    {
+      "id": 2,
+      "quantidade": "0"
+    },
+    {
+      "id": 3,
+      "quantidade": "0"
+    },
+    {
+      "id": 4,
+      "quantidade": "0"
+    },
+    {
+      "id": 5,
+      "quantidade": "0"
+    },
+    {
+      "id": 6,
+      "quantidade": "0"
+    },
+    {
+      "id": 7,
+      "quantidade": "0"
+    },
+    {
+      "id": 8,
+      "quantidade": "0"
+    },
+    {
+      "id": 9,
+      "quantidade": "0"
+    },
+    {
+      "id": 10,
+      "quantidade": "0"
+    },
+    {
+      "id": 11,
+      "quantidade": "0"
+    },
+    {
+      "id": 12,
+      "quantidade": "0"
+    },
+    {
+      "id": 13,
+      "quantidade": "0"
+    },
+    {
+      "id": 14,
+      "quantidade": "0"
+    },
+    {
+      "id": 15,
+      "quantidade": "0"
+    },
+    {
+      "id": 16,
+      "quantidade": "0"
+    },
+    {
+      "id": 17,
+      "quantidade": "0"
+    },
+    {
+      "id": 18,
+      "quantidade": "0"
+    },
+    {
+      "id": 19,
+      "quantidade": "0"
+    },
+    {
+      "id": 20,
+      "quantidade": "0"
+    },
+    {
+      "id": 21,
+      "quantidade": "0"
+    },
+    {
+      "id": 22,
+      "quantidade": "0"
+    },
+    {
+      "id": 23,
+      "quantidade": "0"
+    },
+    {
+      "id": 24,
+      "quantidade": "0"
+    },
+    {
+      "id": 25,
+      "quantidade": "0"
+    },
+    {
+      "id": 26,
+      "quantidade": "0"
+    },
+    {
+      "id": 27,
+      "quantidade": "0"
+    },
+    {
+      "id": 28,
+      "quantidade": "0"
+    },
+    {
+      "id": 29,
+      "quantidade": "0"
+    },
+    {
+      "id": 30,
+      "quantidade": "0"
+    }
+  ]
+}
+
+var ls_ListaSupernosso_inicial = {
+  "data": [
+    {
+      "id": 1,
+      "preço": "7.99"
+    },
+    {
+      "id": 2,
+      "preço": "3.19"
+    },
+    {
+      "id": 3,
+      "preço": "6.79"
+    },
+    {
+      "id": 4,
+      "preço": "4.79"
+    },
+    {
+      "id": 5,
+      "preço": "7.49"
+    },
+    {
+      "id": 6,
+      "preço": "5.59"
+    },
+    {
+      "id": 7,
+      "preço": "3.59"
+    },
+    {
+      "id": 8,
+      "preço": "4.69"
+    },
+    {
+      "id": 9,
+      "preço": "3.89"
+    },
+    {
+      "id": 10,
+      "preço": "3.29"
+    },
+    {
+      "id": 11,
+      "preço": "24.95"
+    },
+    {
+      "id": 12,
+      "preço": "23.99"
+    },
+    {
+      "id": 13,
+      "preço": "15.96"
+    },
+    {
+      "id": 14,
+      "preço": "9.98"
+    },
+    {
+      "id": 15,
+      "preço": "24.90"
+    },
+    {
+      "id": 16,
+      "preço": "7.99"
+    },
+    {
+      "id": 17,
+      "preço": "4.99"
+    },
+    {
+      "id": 18,
+      "preço": "4.99"
+    },
+    {
+      "id": 19,
+      "preço": "8.99"
+    },
+    {
+      "id": 20,
+      "preço": "4.79"
+    },
+    {
+      "id": 21,
+      "preço": "23.99"
+    },
+    {
+      "id": 22,
+      "preço": "2.29"
+    },
+    {
+      "id": 23,
+      "preço": "2.29"
+    },
+    {
+      "id": 24,
+      "preço": "1.99"
+    },
+    {
+      "id": 25,
+      "preço": "26.99"
+    },
+    {
+      "id": 26,
+      "preço": "29.98"
+    },
+    {
+      "id": 27,
+      "preço": "3.19"
+    },
+    {
+      "id": 28,
+      "preço": "11.99"
+    },
+    {
+      "id": 29,
+      "preço": "18.49"
+    },
+    {
+      "id": 30,
+      "preço": "18.99"
+    }
+  ]
+}
+
+var lv_ListaVerdemar_inicial = {
+  "data": [
+    {
+      "id": 1,
+      "preço": "8.50"
+    },
+    {
+      "id": 2,
+      "preço": "3.50"
+    },
+    {
+      "id": 3,
+      "preço": "7.29"
+    },
+    {
+      "id": 4,
+      "preço": "4.99"
+    },
+    {
+      "id": 5,
+      "preço": "7.99"
+    },
+    {
+      "id": 6,
+      "preço": "5.59"
+    },
+    {
+      "id": 7,
+      "preço": "3.89"
+    },
+    {
+      "id": 8,
+      "preço": "4.79"
+    },
+    {
+      "id": 9,
+      "preço": "3.99"
+    },
+    {
+      "id": 10,
+      "preço": "3.19"
+    },
+    {
+      "id": 11,
+      "preço": "30.95"
+    },
+    {
+      "id": 12,
+      "preço": "26.99"
+    },
+    {
+      "id": 13,
+      "preço": "14.96"
+    },
+    {
+      "id": 14,
+      "preço": "10.98"
+    },
+    {
+      "id": 15,
+      "preço": "25.90"
+    },
+    {
+      "id": 16,
+      "preço": "8.99"
+    },
+    {
+      "id": 17,
+      "preço": "5.99"
+    },
+    {
+      "id": 18,
+      "preço": "5.59"
+    },
+    {
+      "id": 19,
+      "preço": "8.09"
+    },
+    {
+      "id": 20,
+      "preço": "4.59"
+    },
+    {
+      "id": 21,
+      "preço": "29.65"
+    },
+    {
+      "id": 22,
+      "preço": "2.79"
+    },
+    {
+      "id": 23,
+      "preço": "2.39"
+    },
+    {
+      "id": 24,
+      "preço": "2.39"
+    },
+    {
+      "id": 25,
+      "preço": "22.05"
+    },
+    {
+      "id": 26,
+      "preço": "24.39"
+    },
+    {
+      "id": 27,
+      "preço": "3.29"
+    },
+    {
+      "id": 28,
+      "preço": "12.15"
+    },
+    {
+      "id": 29,
+      "preço": "19.49"
+    },
+    {
+      "id": 30,
+      "preço": "19.99"
+    }
+  ]
+}
+
+
+var lh_produtos_inicial = {
+  "data": [
+
+  ]
+}
+
+var Item_Carrinho = {
+  "data": [
+
+  ]
+}
+
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var rp = JSON.parse(localStorage.getItem('rp_produto'));
+if (!rp) {
+  rp = rp_produtos_inicial
+};
+
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var la = JSON.parse(localStorage.getItem('lista_ativa'));
+if (!la) {
+  la = rp_ListaAtiva_inicial
+};
+
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var ls = JSON.parse(localStorage.getItem('lista_supernosso'));
+if (!ls) {
+  ls = ls_ListaSupernosso_inicial
+};
+
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var lv = JSON.parse(localStorage.getItem('lista_verdemar'));
+if (!lv) {
+  lv = lv_ListaVerdemar_inicial
+};
+
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var iC = JSON.parse(localStorage.getItem('Item_Carrinho'));
+if (!iC) {
+  iC = Item_Carrinho
+};
+
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var li = JSON.parse(localStorage.getItem('Lista_Indice'));
+if (!li) {
+  li = Lista_Indice
+};
+
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var lh_produtos_inicial = JSON.parse(localStorage.getItem('lista_historico'));
+if (!lh) {
+  lh = lh_produtos_inicial
+};
+
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var lf = JSON.parse(localStorage.getItem('Lista_Favoritos'));
+if (!lf) {
+  lf = Lista_Favoritos
+};
+
+// Exibe mensagem em um elemento de ID msg
+function displayMessage(msg) {
+  $('#msg').html('<div class="alert alert-warning">' + msg + '</div>');
+}
+
+function updateProduto(id, produto) {
+  // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+  let index = la.data.map(obj => obj.id).indexOf(id);
+
+  // Altera os dados do objeto no array
+  la.data[index].quantidade = produto.quantidade
+
+  displayMessage("Produto adicionado à lista");
+
+  // Atualiza os dados no Local Storage
+  localStorage.setItem('lista_ativa', JSON.stringify(la));
+}
+
+function insertProduto(produtoLista) {
+  // Calcula novo Id a partir do último código existente no array (PODE GERAR ERRO SE A BASE ESTIVER VAZIA)
+
+  localStorage.removeItem('Item_Carrinho');
+  iC = Item_Carrinho;
+  let novoId = 1;
+  if (iC.data.length != 0)
+    novoId = iC.data[iC.data.length - 1].id + 1;
+  let novoProduto = {
+    "id": novoId,
+    "nome": produtoLista.nome,
+    "quantidade": produtoLista.quantidade,
+    "preco": produtoLista.preço
+  };
+
+  // Insere o novo objeto no array
+  iC.data.push(novoProduto);
+
+  // Atualiza os dados no Local Storage
+  localStorage.setItem('Item_Carrinho', JSON.stringify(iC));
+}
+
+function insertProdutoFavoritos(produtoLista) {
+  // Calcula novo Id a partir do último código existente no array (PODE GERAR ERRO SE A BASE ESTIVER VAZIA)
+
+  var Lista_Favoritos = {
+    "data": []
+  }
+  let lf = Lista_Favoritos;
+  localStorage.setItem('Lista_Favoritos', JSON.stringify(lf));
+  let novoId = 1;
+  if (lf.data.length != 0)
+    novoId = lf.data[lf.data.length - 1].id + 1;
+  let novoProduto = {
+    "id": novoId,
+    "usuario": produtoLista.usuario,
+    "produto": li.data
+  };
+
+  // Insere o novo objeto no array
+  lf.data.push(novoProduto);
+
+  // Atualiza os dados no Local Storage
+  localStorage.setItem('Lista_Favoritos', JSON.stringify(lf));
+}
+
+function insertProdutoIndice(produtoLista) {
+  // Calcula novo Id a partir do último código existente no array (PODE GERAR ERRO SE A BASE ESTIVER VAZIA)
+
+  var Lista_Indice = {
+    "data": []
+  }
+  let li = Lista_Indice;
+  localStorage.setItem('Lista_Indice', JSON.stringify(li));
+  let novoId = 1;
+  if (li.data.length != 0)
+    novoId = li.data[li.data.length - 1].id + 1;
+  let novoProduto = {
+    "id": novoId,
+    "supermercado": produtoLista.supermercado,
+    "preço_total": produtoLista.preçoTotal,
+    "produto": iC.data
+  };
+
+  // Insere o novo objeto no array
+  li.data.push(novoProduto);
+
+  // Atualiza os dados no Local Storage
+  localStorage.setItem('Lista_Indice', JSON.stringify(li));
+}
+
+function insertProdutoHistorico(produtoLista) {
+  // Calcula novo Id a partir do último código existente no array (PODE GERAR ERRO SE A BASE ESTIVER VAZIA)
+
+  var Lista_Historico = {
+    "data": []
+  }
+  let lh = Lista_Historico;
+  localStorage.setItem('Lista_Historico', JSON.stringify(lh));
+  let novoId = 1;
+  if (lh.data.length != 0)
+    novoId = lh.data[lh.data.length - 1].id + 1;
+  let novoProduto = {
+    "id": novoId,
+    "usuario": produtoLista.usuario,
+    "produto": li.data
+  };
+
+  // Insere o novo objeto no array
+  lh.data.push(novoProduto);
+
+  // Atualiza os dados no Local Storage
+  localStorage.setItem('Lista_Historico', JSON.stringify(lh));
+}
---
+++ code/Code/assets/js/atualizaPreco.js
@@ -0,0 +1,27 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+
+
+$("#enviar").submit(function() {
+  if($("#preço").val()== null || $("#preço").val() ==""){
+      alert('campo vazio');      
+      return false;
+  }
+});
